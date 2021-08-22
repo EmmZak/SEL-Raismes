b231faa6dcee7f824fe2eda0b6a359a27ac01726
EmmZak <e.zakaryan@ubik-ingenierie.com>
2021-08-22 17:17:59 +0200

    replaced leaflet map with google map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,76 +8,26 @@
         <v-col> text </v-col>
       </v-row>
 
+      <!--  
       <v-card-title class="text-h3 justify-center">
         Troc d'heures raismois
-      </v-card-title>
+      </v-card-title> -->
     </v-card>
 
     <!-- map -->
-    <v-row class="green">
-      <v-col class="yellow">
-        ok
-        <v-card class="orange">
-          <l-map
-            :center="center"
-            :zoom="zoom"
-            ref="map"
-            @update:zoom="zoomUpdated"
-            @update:center="centerUpdated"
-            style="height: 350px; width: 75%"
-          >
-            <l-tile-layer :url="url"></l-tile-layer>
-
-            <!-- marker -->
-            <l-marker :lat-lng="marker.coordinates" @click="markerClick">
-              <l-icon ref="icon">
-                <v-icon class="red--text" x-large> mdi-map-marker </v-icon>
-              </l-icon>
-            </l-marker>
-          </l-map>
-        </v-card>
+    <v-row class="green" align="center" justify="center">
+      <v-col class="purple" md="8">
+        <iframe
+          width="100%"
+          height="450"
+          style="border: 0"
+          loading="lazy"
+          allowfullscreen
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDgI1A9YSRRa4AVAZZE5VfFnokytTACUa0&q=50.403587663227206, 3.5099710163715776"
+        >
+        </iframe>
       </v-col>
     </v-row>
-
-    <!-- adresse dialog -->
-    <v-dialog
-      v-model="dialog"
-      max-width="600"
-      style="position: fixed; z-index: 500"
-    >
-      <v-card>
-        <v-toolbar color="primary" dark>
-          Adresse du SEL
-          <v-spacer></v-spacer>
-          <v-btn icon @click="close()">
-            <v-icon color="white">mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar>
-
-        <v-card-text class="pt-8 pb-8">
-          <v-row align="center" justify="center">
-            <v-col class="text-center">
-              <v-icon size="150"> mdi-map-marker </v-icon>
-            </v-col>
-            <v-col>
-              <span class="text-h6">
-                Mairie Annexe De Sabatier <br />
-                Rue Léopold Dussart <br />
-                59590 <br />
-                Raismes
-              </span>
-              <!-- 
-              <div class="text-h4 pa-12 text-wrap hidden-sm-and-down">
-                Rue Léopold Dussart, 59590 Raismes
-              </div>
-              <div class="text-h6 pa-12 text-wrap hidden-md-and-up">
-                Rue Léopold Dussart, 59590 Raismes
-              </div> -->
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
   </v-container>
 
   <!-- Carousel 
@@ -104,17 +54,9 @@
 </template>
 src/
 <script>
-import { LIcon, LMap, LTileLayer, LMarker } from "vue2-leaflet";
-import "leaflet/dist/leaflet.css";
-
 export default {
   name: "Home",
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LIcon,
-  },
+  components: {},
   data() {
     return {
       // map
@@ -149,7 +91,9 @@ export default {
     },
     markerClick() {
       //this.dialog = true;
-      window.open("https://maps.google.com/?q=50.403522477580154,3.509962397737272");
+      window.open(
+        "https://maps.google.com/?q=50.403522477580154,3.509962397737272"
+      );
     },
     zoomUpdated(zoom) {
       this.zoom = zoom;
